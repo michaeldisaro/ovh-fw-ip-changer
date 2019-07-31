d03c1ff8cc062226e0f5d9620e3158ea7c10cd82
MichaelDisaro <michael.disaro@gmail.com>
2019-07-31 21:56:50 +0200

    v1

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # ovh-fw-ip-changer
-A simple script to run if you have dynamic IP address to change the ip in the "ssh access rule" on your OVH firewall with the new one
+A simple script to run if you have dynamic IP address to change the old ip in the "ssh access rule" on your OVH firewall 
+with the new one.
+
+**SETUP**
+
+Clone this repository and run a ***composer install*** command.
+
+Create an OVH API application and authorize it following: https://docs.ovh.com/gb/en/customer/first-steps-with-ovh-api/
+
+Once you have APPLICATION KEY, APPLICATION SECRET and CUSTOMER KEY create a ***config.php*** using the following template:
+```
+<?php
+$config = [
+    "AK" => "1mXkn5umKZUY5bK6",
+    "AS" => "XV5RM7pDt3nGU49te1kEAp08UiKBQP3K",
+    "CK" => "A7y4fpDc0HKNDPo7jrJnpvYx7TX9OYzU",
+    "priority" => 1
+];
+```
+
+AK = APPLICATION KEY, AS = APPLICATION SECRET, CK = CUSTOMER KEY and priority = ssh rule's priority in your FW chain.
+
+**RUN**
+
+Just run ***php ip-changer.php***
+
+**WARNING**
+
+DO NOT LEAVE YOUR CREDENTIALS AROUND! WHO HAS THEM CAN ALLOW SSH TO YOUR MACHINE FOR HIS IP!
--- ip-changer.php
+++ ip-changer.php
@@ -1,1 +1,60 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+include __DIR__ . '/config.php';
+
+use \Ovh\Api;
+
+// Get my current IP
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://api.myip.com/");
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$myIP = json_decode(curl_exec($ch));
+curl_close($ch);
+
+$conn = new Api($config['AK'], $config['AS'], 'ovh-eu', $config['CK']);
+$ips = $conn->get('/ip');
+foreach ($ips as $ipBlock) {
+    if (preg_match('/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/', $ipBlock)) {
+        $url = '/ip/' . urlencode($ipBlock) . '/firewall';
+        $ipUnderFirewall = $conn->get($url, ['enabled' => true])[0];
+
+        echo PHP_EOL. "MANAGING $ipUnderFirewall FIREWALL'S SSH RULE" . PHP_EOL;
+
+        $url .= '/' . $ipUnderFirewall . '/rule';
+
+        // Delete old rule
+        echo "Deleting old ssh rule..." . PHP_EOL;
+        $isPending = true;
+        try {
+            $result = $conn->delete($url . "/{$config['priority']}");
+        } catch (\Exception $ex) {
+            // Usually this goes in error when no rule is present
+            // Uncomment if weird problems occur during deletion of an existing rule
+            //echo $ex->getMessage();
+            $isPending = false;
+        }
+
+        // Wait for pending deletion
+        while ($isPending) {
+            sleep(5);
+            $isPending = @$conn->get($url, ['state' => 'removalPending'])[0] == $config['priority'] ?? false;
+            echo ($isPending ? "Waiting for deletion..." : "Rule 1 has been deleted") . PHP_EOL;
+        }
+        echo "Adding new rule for ip {$myIP->ip}" . PHP_EOL;
+
+        // Add new rule
+        try {
+            $result = $conn->post($url, [
+                'action' => 'permit',
+                'destinationPort' => '22',
+                'protocol' => 'tcp',
+                'sequence' => $config['priority'],
+                'source' => $myIP->ip . '/32'
+            ]);
+            echo "NEW Rule 1 is pending creation..." . PHP_EOL;
+        } catch (\Exception $ex) {
+            echo $ex->getMessage();
+        }
+    }
+}
